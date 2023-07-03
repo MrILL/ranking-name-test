0d7dff483bb3acb491757694b38a4bf2ac2fc107
MrILL <ilua2403@ukr.net>
2023-07-03 12:55:38 +0300

    feat(frontend): init ws interaction with backend

--- frontend/app/list.tsx
+++ frontend/app/list.tsx
@@ -2,31 +2,118 @@
 
 import React from 'react'
 import { Reorder } from 'framer-motion'
+import { io } from 'socket.io-client'
 
 import { Name } from '@/types'
 
 const mock: Name[] = ['MrILL', 'Lepeico', 'Anatoliy', 'Anton']
 
-export const Item = ({ item }: { item: any }) => {
+const URL = 'localhost:3001'
+export const socket = io(URL, {
+  autoConnect: false,
+})
+
+type RawListUnit = {
+  id: number
+  name: Name
+}
+
+const Item = ({
+  rank,
+  data,
+  onDragEnd,
+  onRename,
+  onRenameSubmit,
+  onDelete,
+}: {
+  rank: number
+  data: RawListUnit
+  onDragEnd: () => void
+  onRename: (newName: string) => void
+  onRenameSubmit: (name: string) => void
+  onDelete: () => void
+}) => {
+  const { id, name } = data
+
   return (
-    <Reorder.Item value={item} id={item}>
-      <span>{item}</span>
+    <Reorder.Item
+      value={data}
+      whileDrag={{
+        cursor: 'grabbing',
+      }}
+      onDragEnd={() => onDragEnd()}
+      className="rounded-xl bg-orange-50 shadow p-2 items-center cursor-grab mb-2 flex justify-between group"
+    >
+      <div className="flex gap-3 items-center">
+        <div className="h-7 w-7 flex items-center justify-center text-sm font-bold bg-orange-500 text-white rounded-lg">
+          {rank}
+        </div>
+        <input
+          key={id}
+          value={name}
+          onChange={(e) => onRename(e.currentTarget.value)}
+          onBlur={() => onRenameSubmit(name)}
+          className="border-none bg-transparent p-0 m-0 outline-none font-inherit text-inherit truncate"
+        />
+      </div>
+      <button
+        className="h-7 w-7 flex items-center justify-center rounded-lg transition opacity-0 group-hover:opacity-100 text-orange-800 hover:bg-orange-500/10"
+        onClick={() => onDelete()}
+      >
+        &#10006;
+      </button>
     </Reorder.Item>
   )
 }
 
 export const List = () => {
-  const [list, setList] = React.useState(mock)
+  const [list, setList] = React.useState<RawListUnit[]>([])
   const [newName, setNewName] = React.useState('')
   const [error, setError] = React.useState<string | undefined>(undefined)
 
-  const handleReorder = (newList: Name[]) => {
+  React.useEffect(() => {
+    socket.on('connect', () => {
+      console.log('connect')
+      // socket.emit('ranked-names:get-all')
+    })
+
+    socket.on('ranked-names-updated', (data) => {
+      // TODO types
+      console.log(data)
+      const names = data.map(({ id, name }: any) => ({
+        id,
+        name,
+      }))
+      console.log(names)
+      setList(names)
+    })
+
+    socket.on('error', (errors) => {
+      console.log('errors', errors)
+    })
+
+    socket.connect()
+  }, [])
+
+  const handleReorder = (newList: RawListUnit[]) => {
+    console.log('reorder', newList)
     setList(newList)
-    // TODO call backend to save state
+  }
+
+  const handleOnDragEnd = (id: number, name: string) => {
+    const index = list.findIndex((rankedName) => rankedName.id === id)
+    socket.emit('ranked-names:update', {
+      id,
+      updateDto: {
+        name,
+        prev: list[index - 1]?.name,
+        next: list[index + 1]?.name,
+      },
+    })
   }
 
   const nameIsUnique = (name: string): boolean => {
-    const index = list.findIndex((rankedName) => rankedName === name)
+    const index = list.findIndex((rankedName) => rankedName.name === name)
 
     return index === -1
   }
@@ -38,9 +125,14 @@ export const List = () => {
       return
     }
 
-    setList([...list, newName])
     setNewName('')
-    // TODO call backend to save state
+
+    socket.emit('ranked-names:add', {
+      createDto: {
+        name: newName,
+        prev: list.at(-1)?.name,
+      },
+    })
   }
   console.log(list)
 
@@ -55,54 +147,64 @@ export const List = () => {
     // TODO call backend to save state
   }
 
-  const handleDelete = (name: Name, index: number) => {
-    const updatedList = [...list]
-    updatedList.splice(index, 1)
-    setList(updatedList)
-    // TODO call backend to save state
+  const handleRename = (id: number, name: string) => {
+    const index = list.findIndex((rankedName) => rankedName.id === id)
+
+    const newList = [...list]
+    newList[index].name = name
+    setList(newList)
+  }
+
+  const handleRenameSubmit = (id: number, name: string) => {
+    socket.emit('ranked-names:rename', {
+      id,
+      newName: name,
+    })
+  }
+
+  const handleDelete = (id: number) => {
+    // const updatedList = [...list]
+    // updatedList.splice(index, 1)
+    // setList(updatedList)
+
+    socket.emit('ranked-names:remove', {
+      id,
+    })
   }
 
   return (
     <>
-      <Reorder.Group
-        axis="y"
-        values={list}
-        onReorder={handleReorder}
-        className="relative w-80"
-      >
-        {list.map((rankedName, i) => {
-          return (
-            <Reorder.Item
-              key={rankedName}
-              value={rankedName}
-              whileDrag={{
-                cursor: 'grabbing',
-              }}
-              className="rounded-xl bg-orange-50 shadow p-2 items-center cursor-grab mb-2 flex justify-between group"
-            >
-              <div className="flex gap-3 items-center">
-                <div className="h-7 w-7 flex items-center justify-center text-sm font-bold bg-orange-500 text-white rounded-lg">
-                  {i + 1}
-                </div>
-                {/* TODO contentEditable request on back */}
-                <div
-                  contentEditable
-                  suppressContentEditableWarning
-                  className="text-orange-950"
-                >
-                  {rankedName}
-                </div>
-              </div>
-              <button
-                className="h-7 w-7 flex items-center justify-center rounded-lg transition opacity-0 group-hover:opacity-100 text-orange-800 hover:bg-orange-500/10"
-                onClick={() => handleDelete(rankedName, i)}
-              >
-                &#10006;
-              </button>
-            </Reorder.Item>
-          )
-        })}
-      </Reorder.Group>
+      {!list.length ? (
+        <>
+          <div />
+          <h1 className="font-bold text-center text-2xl">
+            Empty list! Add more names!
+          </h1>
+        </>
+      ) : (
+        <Reorder.Group
+          axis="y"
+          values={list}
+          onReorder={handleReorder}
+          className="relative"
+        >
+          {list.map((rankedName, i) => {
+            const { id, name } = rankedName
+
+            return (
+              <Item
+                key={id}
+                rank={i + 1}
+                data={rankedName}
+                onDragEnd={() => handleOnDragEnd(id, name)}
+                onRename={(newName) => handleRename(id, newName)}
+                onRenameSubmit={(name) => handleRenameSubmit(id, name)}
+                onDelete={() => handleDelete(id)}
+              />
+            )
+          })}
+        </Reorder.Group>
+      )}
       <form
         onSubmit={(e) => {
           e.preventDefault()
